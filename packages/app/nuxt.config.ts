9f960c2f938d7411cfb99158c91a82e9a4b1000a
tristar <tristar.2nd@gmail.com>
2024-10-30 09:29:31 +0900

    Auth0の導入関連

--- packages/app/nuxt.config.ts
+++ packages/app/nuxt.config.ts
@@ -1,18 +1,19 @@
-import { validateToken } from "nuxt-oidc-auth/runtime/server/utils/security.js"
+import { validateToken } from "nuxt-oidc-auth/runtime/server/utils/security.js";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
-  modules: ["@nuxt/eslint", "@pinia/nuxt", "nuxt-auth-utils"],
-  plugins: [],
-  runtimeConfig: {
-    oauth: {
-      auth0: {
-        domain: process.env.NUXT_OAUTH_AUTH0_DOMAIN,
-        clientId: process.env.NUXT_OAUTH_AUTH0_CLIENT_ID,
-        clientSecret: process.env.NUXT_OAUTH_AUTH0_CLIENT_SECRET,
-        redirectURL: process.env.NUXT_OAUTH_AUTH0_REDIRECT_URL,
-      },
-    },
-  },
-})
+	compatibilityDate: "2024-04-03",
+	modules: ["@nuxt/eslint", "@pinia/nuxt", "nuxt-auth-utils"],
+	plugins: [],
+	runtimeConfig: {
+		oauth: {
+			auth0: {
+				domain: process.env.NUXT_OAUTH_AUTH0_DOMAIN,
+				clientId: process.env.NUXT_OAUTH_AUTH0_CLIENT_ID,
+				clientSecret: process.env.NUXT_OAUTH_AUTH0_CLIENT_SECRET,
+				redirectURL: process.env.NUXT_OAUTH_AUTH0_REDIRECT_URL,
+				audience: process.env.NUXT_OAUTH_AUTH0_AUDIENCE,
+			},
+		},
+	},
+});
