92688c80f8a56d53549b7cc87fa87d1ee27d1d66
tristar <tristar.2nd@gmail.com>
2024-11-01 02:52:56 +0900

    serverless.tsの更新

--- packages/api/serverless.ts
+++ packages/api/serverless.ts
@@ -1,7 +1,7 @@
 // Requiring @types/serverless in your project package.json
-import type { Serverless } from "serverless/aws";
+import type { Serverless } from "serverless/aws"
 // serverless.ts
-const serverlessConfiguration: Serverless = {
+const serverlessConfiguration: Serverless & { build: object } = {
   org: "tristar",
   app: "chase-light-dev",
   service: "chase-light-api",
@@ -12,11 +12,8 @@ const serverlessConfiguration: Serverless = {
       bundle: true,
       minify: false,
       buildConcurrency: 3,
-      external: [
-        "aws-lambda",
-        "@prisma/client",
-      ],
-      target: "node20",
+      external: ["aws-lambda", "@prisma/client"],
+      target: "node18",
       platform: "node",
       sourcemap: {
         type: "linked",
@@ -28,19 +25,48 @@ const serverlessConfiguration: Serverless = {
     patterns: [
       "package.json",
       "node_modules/.prisma/client/**",
-      '!node_modules/.prisma/client/libquery_engine-*',
-      'node_modules/.prisma/client/libquery_engine-rhel-*',
+      "!node_modules/.prisma/client/libquery_engine-*",
+      "node_modules/.prisma/client/libquery_engine-rhel-*",
       "node_modules/@prisma/**",
       "!node_modules/@prisma/engines/**",
     ],
   },
   provider: {
     name: "aws",
-    runtime: "nodejs20.x",
+    runtime: "nodejs18.x",
     region: "ap-northeast-1",
+    memorySize: 512,
     environment: {
-      DATABASE_URL: "${env:DATABASE_URL}"
-    }
+      DATABASE_URL:
+        "${env:DATABASE_URL, ssm:/aws/reference/secretsmanager/${sls:stage}/supabase/db_url}}",
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      // NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
+      STAGE: "${sls:stage}",
+    },
+    tracing: {
+      apiGateway: true,
+      lambda: true,
+    },
+    iamRoleStatements: [
+      {
+        Effect: "Allow",
+        Action: [
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:DescribeSecret",
+        ],
+        Resource:
+          "arn:aws:secretsmanager:${env:AWS_REGION}:${env:AWS_ACCOUNT}:secret:${sls:stage}/supabase/db_url",
+      },
+      {
+        Effect: "Allow",
+        Action: ["xray:PutTraceSegments", "xray:PutTelemetryRecords"],
+        Resource: "*",
+      },
+    ],
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
   },
   functions: {
     hello: {
@@ -55,6 +81,6 @@ const serverlessConfiguration: Serverless = {
       ],
     },
   },
-};
+}
 
-module.exports = serverlessConfiguration;
+module.exports = serverlessConfiguration
