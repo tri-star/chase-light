2c8a9f47faef86458f90e4af599375c990e428c2
tristar <tristar.2nd@gmail.com>
2024-11-15 00:03:56 +0900

    Auth0から完全にログアウトするログアウト用関数の追加

---
+++ packages/app/features/auth/composables/use-auth.ts
@@ -0,0 +1,14 @@
+export function useAuth() {
+  async function logout(returnTo?: string) {
+    const config = useRuntimeConfig()
+    const { loggedIn, clear } = useUserSession()
+
+    const logoutUrl = `https://${config.public.auth0.domain}/v2/logout?client_id=${config.public.auth0.clientId}&returnTo=${returnTo}`
+    await clear()
+    return navigateTo(logoutUrl, { external: true })
+  }
+
+  return {
+    logout,
+  }
+}
