94d3ac130c1e9c49116992da49fdfd6cade56bb6
tristar <tristar.2nd@gmail.com>
2024-11-20 00:07:40 +0900

    Nuxt.js側のAPIでダミーのデータを返す実装

--- packages/app/server/api/feeds/logs.get.ts
+++ packages/app/server/api/feeds/logs.get.ts
@@ -27,6 +27,54 @@ export default defineEventHandler(async () => {
           },
         },
       },
+      {
+        id: "1123456780",
+        title: "v0.1.0",
+        url: "https://github.com/",
+        date: new Date(),
+        summary:
+          "GitHub Copilot is an AI pair programmer that helps you write code faster.",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        feed: {
+          id: "1234567890",
+          name: "github/copilot",
+          cycle: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          dataSource: {
+            id: "2345678901",
+            name: "GitHub",
+            url: "https://github.com",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        },
+      },
+      {
+        id: "1123456781",
+        title: "v0.1.0",
+        url: "https://github.com/",
+        date: new Date(),
+        summary:
+          "GitHub Copilot is an AI pair programmer that helps you write code faster.",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        feed: {
+          id: "1234567890",
+          name: "github/copilot",
+          cycle: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          dataSource: {
+            id: "2345678901",
+            name: "GitHub",
+            url: "https://github.com",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        },
+      },
     ] satisfies FeedLog[],
   }
 })
