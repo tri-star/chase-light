49a5a6800a799169087ba05f8e094749ae8ae862
tristar <tristar.2nd@gmail.com>
2024-11-24 10:48:02 +0900

    ボタンコンポーネントの追加

---
+++ packages/app/components/common/A3Button.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { tv } from 'tailwind-variants';
+
+defineProps<{
+  label: string
+}>()
+
+const buttonClasses = tv({
+  base: [
+    'flex',
+    'bg-default',
+    'hover:bg-default-hover',
+    'text-default',
+    'rounded',
+    'px-4',
+    'py-3',
+    'gap-1',
+    'justify-center',
+    'items-center',
+    'shadow-lg',
+    'active:shadow-none',
+    'transition-all',
+    'duration-300',
+    'h-min',
+    'translate-y-[-4px]',
+    'active:translate-y-0',
+  ],
+  variants: {
+    type: {
+      primary: [
+        'bg-primary',
+        'hover:bg-primary-hover',
+        'text-primary',
+      ],
+    },
+    disabled: {
+        true: [
+          'bg-disabled',
+          'hover:bg-disabled',
+          'text-disabled',
+          'translate-y-0',
+          'shadow-none',
+          'cursor-not-allowed',
+        ]
+      }
+  }
+})
+
+</script>
+
+<template>
+  <button :class="buttonClasses({type: 'primary'})">
+    <span class="font-label text-size-m">{{ label }}</span>
+  </button>
+</template>
+
+<style scoped>
+</style>
--- packages/app/pages/index.vue
+++ packages/app/pages/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import A3Button from '~/components/common/A3Button.vue';
 import { SIDE_MENU_ITEM_MAP } from '~/components/common/side-menu/side-menu'
 import LogCard from '~/components/feed/LogCard.vue'
 
@@ -11,13 +12,18 @@ const { data: feedLogs } = useFetch('/api/feeds/logs')
 </script>
 
 <template>
-  <h1>新着情報</h1>
+  <div class="flex flex-col gap-6">
+    <div class="flex items-center">
+      <h1 class="flex-1">新着情報</h1>
+      <A3Button label="フィード登録" />
+    </div>
 
-  <div class="flex flex-col items-center gap-4">
-    <LogCard
-      v-for="feedLog in feedLogs?.result ?? []"
-      :feed-log="feedLog"
-      :key="feedLog.id"
-    />
+    <div class="flex flex-col items-center gap-4">
+      <LogCard
+        v-for="feedLog in feedLogs?.result ?? []"
+        :feed-log="feedLog"
+        :key="feedLog.id"
+      />
+    </div>
   </div>
 </template>
