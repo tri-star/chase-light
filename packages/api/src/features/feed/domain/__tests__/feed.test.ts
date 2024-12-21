92a02145371c3ef5591a1658a51793e078dd3988
tristar <tristar.2nd@gmail.com>
2024-12-22 02:18:28 +0900

    フィードのURL判定のAPI/テストを追加

---
+++ packages/api/src/features/feed/domain/__tests__/feed.test.ts
@@ -0,0 +1,24 @@
+import { describe, expect, test } from "vitest"
+import { isSupportedDataSource } from "../feed"
+
+describe("isSupportedDataSource", () => {
+  test("サポートされているGitHub URLの場合はtrueを返すこと", () => {
+    const url = "https://github.com/owner/repo"
+    expect(isSupportedDataSource(url)).toBe(true)
+  })
+
+  test("スキームがhttpの場合エラーとなること", () => {
+    const url = "http://github.com/owner/repo"
+    expect(isSupportedDataSource(url)).toBe(false)
+  })
+
+  test("サポートされていないURLの場合はfalseを返すこと", () => {
+    const url = "https://example.com/owner/repo"
+    expect(isSupportedDataSource(url)).toBe(false)
+  })
+
+  test("無効なURLの場合はfalseを返すこと", () => {
+    const url = "invalid-url"
+    expect(isSupportedDataSource(url)).toBe(false)
+  })
+})
--- packages/api/src/features/feed/domain/feed.ts
+++ packages/api/src/features/feed/domain/feed.ts
@@ -53,6 +53,42 @@ export const feedSearchResultSchema = z.object({
   pageSize: z.number(),
 })
 
+export const validateFeedUrlRequestSchema = z.object({
+  url: z.string(),
+})
+
+export const FEED_VALIDATE_ERROR_DUPLICATE = "duplicated"
+export const FEED_VALIDATE_ERROR_NOT_SUPPORTED = "not-supported"
+export const validateFeedUrlResponseSchema = z.object({
+  success: z.boolean(),
+  code: makeEnumFromArray([
+    FEED_VALIDATE_ERROR_DUPLICATE,
+    FEED_VALIDATE_ERROR_NOT_SUPPORTED,
+  ]).optional(),
+})
+export type ValidateFeedUrlResponse = z.infer<
+  typeof validateFeedUrlResponseSchema
+>
+
+export function isSupportedDataSource(url: string) {
+  try {
+    const urlObject = new URL(url)
+    const hostName = urlObject.hostname
+
+    if (urlObject.protocol !== "https:") {
+      return false
+    }
+    if (hostName !== "github.com") {
+      return false
+    }
+  } catch (e: unknown) {
+    console.debug(`isSupportedDataSource: ${e}`)
+    return false
+  }
+
+  return true
+}
+
 /**
  * フィードのURLからデータソースのURLを抽出する
  * @param url
@@ -66,6 +102,10 @@ export function extractDataSourceUrl(url: string) {
 
   const ownerName = pathName.split("/")[1] ?? undefined
   const repoName = pathName.split("/")[2] ?? undefined
+
+  if (ownerName == null || repoName == null) {
+    throw new Error(`URLの形式が無効です: ${url}`)
+  }
 
   const dataSourceUrl = `https://${hostName}${ownerName}/${repoName}`
   return dataSourceUrl
