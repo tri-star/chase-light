b7251d54f19a2477f7b647cc9ae4f425df4a96e0
tristar <tristar.2nd@gmail.com>
2025-01-24 09:52:06 +0900

    format適用

--- packages/api/vitest.config.ts
+++ packages/api/vitest.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from "vitest/config"
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     watch: false,
-    include: ["./src/**/*.test.ts"],
-    globalSetup: ["./src/lib/vitest/global-setup-test-db.ts"],
-    setupFiles: ["./src/lib/vitest/setup-test-db.ts"],
+    include: ['./src/**/*.test.ts'],
+    globalSetup: ['./src/lib/vitest/global-setup-test-db.ts'],
+    setupFiles: ['./src/lib/vitest/setup-test-db.ts'],
     hookTimeout: 100 * 1000,
     sequence: {
-      hooks: "list",
+      hooks: 'list',
     },
-    pool: "threads",
+    pool: 'threads',
     poolOptions: {
       threads: {
         singleThread: true,
@@ -21,7 +21,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": "/src",
+      '@': '/src',
     },
   },
 })
