c62db364300b8a9bbfe0377a54002af1da066888
tristar <tristar.2nd@gmail.com>
2025-02-13 09:35:17 +0900

    リリースの件数が多すぎる場合数を制限するように変更

--- packages/api/src/features/feed/services/__tests__/github_release_finder.test.ts
+++ packages/api/src/features/feed/services/__tests__/github_release_finder.test.ts
@@ -41,6 +41,115 @@ describe('GitHubReleaseFinder', () => {
       },
     ])
   })
+  test('日付の昇順で取得していること', async () => {
+    const gitHubApiClientStub = new GitHubApiClientStub()
+    gitHubApiClientStub.setReleaseResponse([
+      {
+        id: 3,
+        name: 'v1.0.2',
+        tag_name: 'v1.0.2',
+        published_at: '2021-01-01T00:00:02Z',
+      },
+      {
+        id: 1,
+        name: 'v1.0.0',
+        tag_name: 'v1.0.0',
+        published_at: '2021-01-01T00:00:00Z',
+      },
+      {
+        id: 2,
+        name: 'v1.0.1',
+        tag_name: 'v1.0.1',
+        published_at: '2021-01-01T00:00:01Z',
+      },
+    ])
+
+    const finder = new GitHubReleaseFinder(gitHubApiClientStub)
+    const result = await finder.list(
+      'owner',
+      'repo',
+      new Date('2020-12-31T00:00:00Z'),
+    )
+
+    expect(result).toEqual([
+      {
+        id: 1,
+        name: 'v1.0.0',
+        publishedAt: new Date('2021-01-01T00:00:00.000Z'),
+      },
+      {
+        id: 2,
+        name: 'v1.0.1',
+        publishedAt: new Date('2021-01-01T00:00:01.000Z'),
+      },
+      {
+        id: 3,
+        name: 'v1.0.2',
+        publishedAt: new Date('2021-01-01T00:00:02.000Z'),
+      },
+    ])
+  })
+  test('limit件数を超える場合、最後のlimit件を取得すること', async () => {
+    const gitHubApiClientStub = new GitHubApiClientStub()
+    gitHubApiClientStub.setReleaseResponse([
+      {
+        id: 1,
+        name: 'v1.0.0',
+        tag_name: 'v1.0.0',
+        published_at: '2021-01-01T00:00:00Z',
+      },
+      {
+        id: 2,
+        name: 'v1.0.1',
+        tag_name: 'v1.0.1',
+        published_at: '2021-01-01T00:00:01Z',
+      },
+      {
+        id: 3,
+        name: 'v1.0.2',
+        tag_name: 'v1.0.2',
+        published_at: '2021-01-01T00:00:02Z',
+      },
+      {
+        id: 4,
+        name: 'v1.0.3',
+        tag_name: 'v1.0.3',
+        published_at: '2021-01-01T00:00:03Z',
+      },
+      {
+        id: 5,
+        name: 'v1.0.4',
+        tag_name: 'v1.0.4',
+        published_at: '2021-01-01T00:00:04Z',
+      },
+    ])
+
+    const finder = new GitHubReleaseFinder(gitHubApiClientStub)
+    const result = await finder.list(
+      'owner',
+      'repo',
+      new Date('2021-01-01T00:00:01Z'),
+      3,
+    )
+
+    expect(result).toEqual([
+      {
+        id: 3,
+        name: 'v1.0.2',
+        publishedAt: new Date('2021-01-01T00:00:02.000Z'),
+      },
+      {
+        id: 4,
+        name: 'v1.0.3',
+        publishedAt: new Date('2021-01-01T00:00:03.000Z'),
+      },
+      {
+        id: 5,
+        name: 'v1.0.4',
+        publishedAt: new Date('2021-01-01T00:00:04.000Z'),
+      },
+    ])
+  })
   test('nameがnullの場合はtag_nameを名前として採用すること', async () => {
     const gitHubApiClientStub = new GitHubApiClientStub()
     gitHubApiClientStub.setReleaseResponse([
