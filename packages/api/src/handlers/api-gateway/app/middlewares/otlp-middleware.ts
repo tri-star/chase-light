5bfa84beb01f72fc705b3b2d335eae05afc38632
tristar <tristar.2nd@gmail.com>
2025-02-22 22:50:49 +0900

    独自のpropagation処理を削除

--- packages/api/src/handlers/api-gateway/app/middlewares/otlp-middleware.ts
+++ packages/api/src/handlers/api-gateway/app/middlewares/otlp-middleware.ts
@@ -1,8 +1,8 @@
 import type { AppContext } from '@/handlers/api-gateway/app/chase-light-app'
 import {
-  context,
+  // context,
   trace,
-  propagation,
+  // propagation,
   SpanKind,
   SpanStatusCode,
 } from '@opentelemetry/api'
@@ -10,25 +10,24 @@ import { createMiddleware } from 'hono/factory'
 
 export const otlpMiddleware = createMiddleware<AppContext>(async (c, next) => {
   // _X_AMZN_TRACE_ID  Root=1-67b8ac15-4a9950272235fb671129bc6b;Parent=722a02e746cff02d;Sampled=1;Lineage=1:cb371678:0
-  const textMapGetter = {
-    keys: (carrer: object) => Object.keys(carrer),
-    get: (
-      carrier: Record<string, string | string[] | undefined>,
-      key: string,
-    ) => carrier[key],
-  }
+  // const textMapGetter = {
+  //   keys: (carrer: object) => Object.keys(carrer),
+  //   get: (
+  //     carrier: Record<string, string | string[] | undefined>,
+  //     key: string,
+  //   ) => carrier[key],
+  // }
   console.log('request headers', c.req.raw.headers)
-  const newContext = propagation.extract(
-    context.active(),
-    c.req.raw.headers,
-    textMapGetter,
-  )
+  // const newContext = propagation.extract(
+  //   context.active(),
+  //   c.req.raw.headers,
+  //   textMapGetter,
+  // )
   await trace.getTracer('API').startActiveSpan(
     'API',
     {
       kind: SpanKind.SERVER,
     },
-    newContext,
     async (span) => {
       try {
         console.log(span.spanContext())
