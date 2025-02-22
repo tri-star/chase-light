50389b7253aa4b9ae194e13bee5680b7dcae782d
tristar <tristar.2nd@gmail.com>
2025-02-22 10:30:01 +0900

    propagationしない方法を試す

--- packages/api/src/handlers/api-gateway/app/middlewares/otlp-middleware.ts
+++ packages/api/src/handlers/api-gateway/app/middlewares/otlp-middleware.ts
@@ -62,28 +62,29 @@ export const otlpMiddleware = createMiddleware<AppContext>(async (c, next) => {
       .finally(() => process.exit(0))
   })
 
-  console.log(process.env)
-  const textMapGetter = {
-    keys: (carrer: object) => Object.keys(carrer),
-    get: (
-      carrier: Record<string, string | string[] | undefined>,
-      key: string,
-    ) => carrier[key],
-  }
-  const newContext = propagation.extract(
-    context.active(),
-    process.env,
-    textMapGetter,
-  )
-  console.log('new context', newContext)
+  // _X_AMZN_TRACE_ID  Root=1-67b8ac15-4a9950272235fb671129bc6b;Parent=722a02e746cff02d;Sampled=1;Lineage=1:cb371678:0
+  // const textMapGetter = {
+  //   keys: (carrer: object) => Object.keys(carrer),
+  //   get: (
+  //     carrier: Record<string, string | string[] | undefined>,
+  //     key: string,
+  //   ) => carrier[key],
+  // }
+  // const newContext = propagation.extract(
+  //   context.active(),
+  //   process.env,
+  //   textMapGetter,
+  // )
+  console.log('honoContext', c.req.raw.headers)
   await trace.getTracer('API').startActiveSpan(
     'API',
     {
       kind: SpanKind.SERVER,
     },
-    newContext,
+    // newContext,
     async (span) => {
       try {
+        console.log(span.spanContext())
         await next()
       } catch (e) {
         span.setStatus({
