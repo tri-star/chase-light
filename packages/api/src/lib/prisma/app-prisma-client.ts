efbce61ca1a90adfb50458494b9f267b6bc458ac
tristar <tristar.2nd@gmail.com>
2025-02-24 09:56:01 +0900

    独自のPrismaクエリログ機構を廃止

--- packages/api/src/lib/prisma/app-prisma-client.ts
+++ packages/api/src/lib/prisma/app-prisma-client.ts
@@ -1,19 +1,6 @@
-import { SpanKind, trace } from '@opentelemetry/api'
 import { PrismaClient } from '@prisma/client'
-import dayjs from 'dayjs'
 
 let prismaClient: PrismaClient | undefined
-const getQueryWithParams = (query: string, params: string[]) => {
-  let i = 1
-  while (query.indexOf('$') >= 0) {
-    query = query.replace(
-      query[query.indexOf('$')] + query[query.indexOf('$') + 1],
-      `"${params[i - 1]}"`,
-    )
-    i++
-  }
-  return query
-}
 
 export function getPrismaClientInstance() {
   if (!prismaClient) {
@@ -24,29 +11,6 @@ export function getPrismaClientInstance() {
           level: 'query',
         },
       ],
-    })
-
-    // @ts-expect-error なぜか、'query'イベント名の型がneverとなってしまう
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    prismaClient.$on('query', (event: Record<string, any>) => {
-      const startTime = new Date(event.timestamp)
-      const endTime = dayjs(event.timestamp.valueOf())
-        .add(event.duration, 'ms')
-        .toDate()
-
-      const span = trace.getTracer('prisma').startSpan('Prisma', {
-        startTime,
-        kind: SpanKind.INTERNAL,
-      })
-      const query = getQueryWithParams(event.query, JSON.parse(event.params))
-      // console.dir(query, { depth: 10 })
-      // console.log(span)
-      span.addEvent('query', {
-        query,
-      })
-      span.setAttribute('query', query)
-      span.setAttribute('duraiton', event.duration)
-      span.end(endTime)
     })
   }
   return prismaClient
