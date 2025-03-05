e493682a35ddb1cac4338949ef2e58f583cb939d
tristar <tristar.2nd@gmail.com>
2025-03-05 23:52:17 +0900

    devcontainers内からDBを利用したテストを実行するための変更

--- packages/api/vitest.config.ts
+++ packages/api/vitest.config.ts
@@ -4,7 +4,6 @@ export default defineConfig({
   test: {
     watch: false,
     include: ['./src/**/*.test.ts'],
-    globalSetup: ['./src/lib/vitest/global-setup-test-db.ts'],
     setupFiles: ['./src/lib/vitest/setup-test-db.ts'],
     hookTimeout: 100 * 1000,
     sequence: {
