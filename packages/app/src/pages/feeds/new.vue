7cbeb356297ed4bf8e63c72144fb8fff11ebb0e8
tristar <tristar.2nd@gmail.com>
2025-03-08 16:27:19 +0900

    フィード新規作成画面のコードの置き場所を見直し

--- packages/app/src/pages/feeds/new.vue
+++ packages/app/src/pages/feeds/new.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { SIDE_MENU_ITEM_MAP } from '~/components/common/side-menu/side-menu'
-import FeedCreatePage from '~/features/feed/pages/feed-form/FeedCreatePage.vue'
+import FeedCreatePage from '~/components/pages/feeds/form/FeedCreatePage.vue'
 
 definePageMeta({
   allowGuest: false,
