d8985c51618199571f3f268211f1fcdb6aac2ab7
tristar <tristar.2nd@gmail.com>
2025-06-08 21:12:05 +0900

    Lintエラー修正

--- packages/frontend/eslint.config.mjs
+++ packages/frontend/eslint.config.mjs
@@ -1,5 +1,4 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
 
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs';
