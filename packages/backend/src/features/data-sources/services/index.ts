c9e1bc9f6d7b4304590399378c616dfa3fe325ce
tristar <tristar.2nd@gmail.com>
2025-11-25 02:31:05 +0900

    未使用なはずのファイルの削除

--- packages/backend/src/features/data-sources/services/index.ts
+++ packages/backend/src/features/data-sources/services/index.ts
@@ -1,8 +1,2 @@
 // データソースサービスのエクスポート
-export * from "./data-source-creation.service"
-export * from "./data-source-watch.service"
-export * from "./data-source-list.service"
-export * from "./data-source-detail.service"
-export * from "./data-source-update.service"
-export * from "./data-source-deletion.service"
 export * from "./github-api.service"
